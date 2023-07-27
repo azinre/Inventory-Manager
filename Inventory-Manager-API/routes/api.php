cd8a04bf7d54cf4d337904e0e8ab0d5474c66e5b
azinre <azin.rg@gmail.com>
2023-07-27 16:12:14 -0400

    Provider completed

--- Inventory-Manager-API/routes/api.php
+++ Inventory-Manager-API/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProviderController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/providers', [ProviderController::class, 'index']);
+Route::get('/providers/{id}', [ProviderController::class, 'show']);
+Route::post('/providers', [ProviderController::class, 'create']);
+Route::put('/providers/{id}', [ProviderController::class, 'update']);
+Route::delete('/providers/{id}', [ProviderController::class, 'destroy']);
+
+
+// Route::prefix('providers')->group(function () {
+    
+//     Route::get('/', [ProviderController::class, 'index']);
+
+   
+//     Route::get('/{provider}', [ProviderController::class, 'show']);
+
+
+//     Route::post('/', [ProviderController::class, 'store']);
+
+    
+//     Route::put('/providers/{provider}', [ProviderController::class, 'update']);
+
+
+    
+//     Route::delete('/{provider}', [ProviderController::class, 'destroy']);
+// });
