4cd2c17f68965e90913cc0cac8bc2bc9200600d9
Minaa2022 <amin0057@algonquinlive.com>
2023-07-27 14:10:15 -0400

    updated1

--- Inventory-Manager-API/routes/api.php
+++ Inventory-Manager-API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/', [ItemController::class, 'index']);
+Route::post('/', [ItemController::class, 'show']);
+Route::get('/', [ItemController::class, 'index']);
+Route::get('/', [ItemController::class, 'index']);
