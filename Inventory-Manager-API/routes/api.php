489a5c5cadf5b840398d52e9007e5b1b23cf1b4f
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2023-07-27 13:57:07 -0400

    client controller

--- Inventory-Manager-API/routes/api.php
+++ Inventory-Manager-API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +10,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/clients',[ClientController::class, 'index']);
+
+Route::get('/clients/{id}',[ClientController::class, 'show']);
+
+Route::post('/clients',[ClientController::class, 'create']);
+
+Route::put('/clients/{id}',[ClientController::class, 'update']);
+
+Route::delete('/clients/{id}',[ClientController::class, 'destroy']);
